0199a206ee37a6c14b4dabddb59059b38ae3e756
Alaa Abdelhamid <aabdelh4@vols.utk.edu>
2023-07-20 09:48:22 -0400

    Update display_event_x.py

--- Validating Events/display_event_x.py
+++ Validating Events/display_event_x.py
@@ -7,14 +7,15 @@ with pyhepmc.open(filename) as f:
     # Loop over events in the file
     for i, event in enumerate(f):
         # Save a plot of the event
-        if event.event_number == 11:
+        if event.event_number == 868 or i ==868:
           savefig(event, f"event{i}.svg")
           savefig(event, f"event{i}.png")
           savefig(event, f"event{i}.pdf")
           for particle in event.particles:
-            if particle.id ==1399 and particle.pid==11:
+            if particle.id ==552 and particle.pid==-211:
               ver=particle.production_vertex
               print(ver)
+              print("i: ",i, "    event#:   ", event.event_number)
 
 
 
