a9a2db628a4ab853373a710e9bd400e83ae12612
Alaa Abdelhamid <130788379+A-A-Abdelhamid@users.noreply.github.com>
2023-08-29 12:53:01 -0400

    Update Selection&Eff.py

--- Selection Cuts/Selection&Eff.py
+++ Selection Cuts/Selection&Eff.py
@@ -11,6 +11,59 @@ from ROOT import TLorentzVector
 from ROOT import TEfficiency, TFile, TH1F, TGraph2DErrors, gStyle, gROOT, TColor, TLatex
 import random
 
+import json
+
+from array import array
+
+# Number of points in gradient
+nRGBs = 5
+
+# Stop points at 0, 0.2, 0.3, 0.4, and 0.6
+stops = array('d', [0.0, 0.2, 0.3, 0.4, 0.6])
+
+# RGB values for the colors at the stops (scaled between 0 and 1)
+red   = array('d', [236/255.0, 97/255.0, 33/255.0, 30/255.0, 21/255.0])
+green = array('d', [228/255.0, 203/255.0, 191/255.0, 172/255.0, 121/255.0])
+blue  = array('d', [241/255.0, 221/255.0, 212/255.0, 190/255.0, 133/255.0])
+
+# Create the gradient color table
+ROOT.TColor.CreateGradientColorTable(nRGBs, stops, red, green, blue, 255)
+ROOT.gStyle.SetNumberContours(255)
+hist =     ROOT.TH2F("hist", "hist",10, 65, 765, 8, 0, 400)
+hist.SetStats(0)
+
+Pt = []
+d0 = []
+eff = []
+
+# Read the JSON file
+file_path = "2.json"
+with open(file_path, 'r') as f:
+    json_data = json.load(f)
+    
+values_data = json_data.get('values', None)
+
+for entry in values_data:
+    Pt.append(float(entry['x'][0]['value']))
+    d0.append(float(entry['x'][1]['value']))
+    eff.append(float(entry['y'][0]['value']))
+
+# Fill the histogram
+for i in range(len(Pt)):
+    # Adding 1 to ensure we start from bin 1
+    x_bin = hist.GetXaxis().FindBin(Pt[i])
+    y_bin = hist.GetYaxis().FindBin(d0[i])
+    hist.SetBinContent(x_bin, y_bin, eff[i])
+
+# Draw the histogram
+c = ROOT.TCanvas("canvas", "Pt-do eff", 800, 600)
+hist.GetXaxis().SetTitle("Pt (GeV)")
+hist.GetYaxis().SetTitle("d0 (mm)")
+hist.SetTitle("Muon reconstruction efficiency")
+hist.Draw("COLZ")
+c.Update()
+c.SaveAs("code.pdf")
+
 def CalcEta(particle):
   """
   Calculate eta of a particle
@@ -65,7 +118,7 @@ root_file = TFile.Open(file_path)
 directory = root_file.GetDirectory("pt-d0 muon efficiency")
 graph = directory.Get("Graph2D_y1")
 
-hist= graph.GetHistogram()
+#hist= graph.GetHistogram()
 
 def eff_func (lepton1, lepton2):
 
@@ -77,19 +130,18 @@ def eff_func (lepton1, lepton2):
   
   
 
+ 
   binX1 = hist.GetXaxis().FindBin(x1)
   binY1 = hist.GetYaxis().FindBin(y1)
-  if binY1 ==0:
-    binY1=1
-    
-    
+  
   binX2 = hist.GetXaxis().FindBin(x2)
   binY2 = hist.GetYaxis().FindBin(y2)
-  if binY2 ==0:
-    binY2=1
-    
+  
   eff_value1= hist.GetBinContent(binX1, binY1)
   eff_value2= hist.GetBinContent(binX2, binY2)
+  if eff_value1 ==0:
+   print(x1,y1,binX1,binY1)
+  #print(eff_value1, eff_value2)
   
   rand1= random.random()
   rand2= random.random()
@@ -142,7 +194,8 @@ with hep.open(hepmc_file) as f:
           
       leptons.sort(key=lambda lepton: -lepton.momentum.pt())
 
-        
+        # Select the top two leptons (if there are at least two)
+      #if len(leptons) >= 2:
         
       if len(leptons) == 2:
         
@@ -218,11 +271,12 @@ with hep.open(hepmc_file) as f:
                   break
                   
                   
-    if len(leptons)>=2:
-      print(eff_func(leptons[0],leptons[1]), good_event)
+   
         
         
-print(good_event)
+print("Number of passed events is: ", good_event, " event")
+print("Number of expected events: ", (good_event* 0.0005221*139*1000)/20000)
+
 
               
   
