f01812a48b085f8caa42f670ec7d9dedd82a9808
j-s-ashley <jashley6@vols.utk.edu>
2023-12-26 21:39:26 +0100

    Added variable parameters to display_event_x.py

--- event_validation/display_event_x.py
+++ event_validation/display_event_x.py
@@ -1,25 +1,23 @@
 import pyhepmc
 from pyhepmc.view import savefig
 
-filename = "tag_1_pythia8_events.hepmc"
-# pyhepmc.open can read most HepMC formats using auto-detection
+filename = "../../run_data/run_17/Events/run_01/tag_1_pythia8_events.hepmc"
+
+event_num = 868 # Event number to plot
+p_id = 1399 # Particle index number in record
+p_pid = -211 # PDG ID of particle
+
 with pyhepmc.open(filename) as f:
     # Loop over events in the file
     for i, event in enumerate(f):
         # Save a plot of the event
-        if event.event_number == 868 or i ==868:
+        if event.event_number == event_num or i == event_num:
           savefig(event, f"event{i}.svg")
           savefig(event, f"event{i}.png")
           savefig(event, f"event{i}.pdf")
           for particle in event.particles:
-            if particle.id ==1399 and particle.pid==-211:
-              ver=particle.production_vertex
+            if particle.id == p_id and particle.pid == p_pid:
+              ver = particle.production_vertex
               print(ver)
               print("i: ",i, "    event#:   ", event.event_number)
-
-
-
-   
-
-      
 
