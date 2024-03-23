7e029d51dded49fa6032e5c100c703fb034a89f9
j-s-ashley <jashley6@vols.utk.edu>
2024-03-23 23:40:38 +0100

    Adds .yaml input to smuon_acceptance_compary.py

--- analysis/smuon_acceptance_compare.py
+++ analysis/smuon_acceptance_compare.py
@@ -1,7 +1,16 @@
 import numpy as np
 import pyhepmc as hep
+import yaml
 
-og_file = "HEPData-ins1831504-v2-Smuon_acceptance.csv"
-og_data = np.genfromtxt(og_file, delimiter=',')
+# --- CSV ---
+#og_file = "HEPData-ins1831504-v2-Smuon_acceptance.csv"
+#og_data = np.genfromtxt(og_file, delimiter=',')
 
-print(og_data)
+# --- YAML ---
+og_file = "HEPData-ins1831504-v2-Smuon_acceptance.yaml"
+with open(og_file, 'r') as f:
+    og_data = yaml.load(f, Loader=yaml.SafeLoader) 
+
+acceptance = og_data.get('dependent_variables', {})[0].get('values')
+
+print(acceptance)
