24d7aed6fc5c16b4fb67e3ac19ab8e020c4cf510
j-s-ashley <jashley6@vols.utk.edu>
2024-04-21 11:00:51 +0200

    Adds functionality comments to make_pythia_script.sh to avoid repeats of Issue #30

--- generate_events/make_pythia_script.sh
+++ generate_events/make_pythia_script.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 # To use, replace the file names at the end of lines 5 and 6 with your file name.
 # If compiling a script for HepMC output, uncomment the last 5 lines.
+
+# Run compiler
 /cvmfs/sft.cern.ch/lcg/releases/gcc/11.3.0-ad0f5/x86_64-centos9/bin/g++ \
+	# Pass path for to-be-compiled file to compiler
 	/eos/home-j/jashley/LLP_Sleptons_RPV_SUSY/generate_events/main38.cc \
+	# Specify output
 	-o /eos/home-j/jashley/LLP_Sleptons_RPV_SUSY/generate_events/main38 \
+	# Reference the Pythia default configuration files
 	-I/cvmfs/sft.cern.ch/lcg/releases/MCGenerators/pythia8/308-3f34f/x86_64-centos9-gcc11-opt/include \
+	# Include Pythia's default flags
 	-O2 -std=c++11 -pedantic -W -Wall -Wshadow -fPIC -pthread -DGZIP -lz \
+	# Reference the primary Pythia libraries
 	-L/cvmfs/sft.cern.ch/lcg/releases/MCGenerators/pythia8/308-3f34f/x86_64-centos9-gcc11-opt/lib \
+	# Specifically look for pythia8.so
 	-Wl,-rpath,/cvmfs/sft.cern.ch/lcg/releases/MCGenerators/pythia8/308-3f34f/x86_64-centos9-gcc11-opt/lib \
 	-lpythia8 -ldl \
+	# Repeat the process from lines 15-19 for HepMC
 #	-I/cvmfs/sft.cern.ch/lcg/releases/HepMC/2.06.11-d5a39/x86_64-centos9-gcc11-opt/include \
 #	-L/cvmfs/sft.cern.ch/lcg/releases/HepMC/2.06.11-d5a39/x86_64-centos9-gcc11-opt/lib \
 #	-Wl,-rpath,/cvmfs/sft.cern.ch/lcg/releases/HepMC/2.06.11-d5a39/x86_64-centos9-gcc11-opt/lib \
