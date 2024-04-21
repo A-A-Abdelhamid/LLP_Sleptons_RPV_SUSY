2b8fd86e62892562dfa356689355b9c276256677
j-s-ashley <jashley6@vols.utk.edu>
2024-04-21 11:41:56 +0200

    Adds path references to make_pythia_script.sh and fixes comments

--- generate_events/make_pythia_script.sh
+++ generate_events/make_pythia_script.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-# TO USE: replace the file names at the end of lines 8 and 10 with your file name.
 # If compiling a script for HepMC output, uncomment the last 5 lines.
 
-# 11) Run compiler; 12) pass path for to-be-compiled file to compiler; 
-# 13) specify output; 14) reference Pythia default config files;
-# 15) include Pythia default flags; 16) Reference Pythia libraries;
-# 17-18) specifically look for pythia8.so;
-# 19-23) repeat the process from lines 14-18 for HepMC
+# Line-by-line breakdown:
+# 11-12) Get file name from user; 15) Run compiler; 
+# 16) pass path for to-be-compiled file to compiler; 
+# 17) specify output; 18) reference Pythia default config files;
+# 19) include Pythia default flags; 20) Reference Pythia libraries;
+# 21-22) specifically look for pythia8.so;
+# 23-26) repeat the process from lines 20-22 for HepMC; 27) ???
 
 echo Enter file name, excluding the extension:
 read filename
 
-/cvmfs/sft.cern.ch/lcg/releases/gcc/11.3.0-ad0f5/x86_64-centos9/bin/g++ \
-	/eos/home-j/jashley/LLP_Sleptons_RPV_SUSY/generate_events/$filename.cc \
-	-o /eos/home-j/jashley/LLP_Sleptons_RPV_SUSY/generate_events/$filename \
-	-I/cvmfs/sft.cern.ch/lcg/releases/MCGenerators/pythia8/308-3f34f/x86_64-centos9-gcc11-opt/include \
+$CXX \
+	$PWD/$filename.cc \
+	-o $PWD/$filename \
+	-I$PYTHIA8/include \
 	-O2 -std=c++11 -pedantic -W -Wall -Wshadow -fPIC -pthread -DGZIP -lz \
-	-L/cvmfs/sft.cern.ch/lcg/releases/MCGenerators/pythia8/308-3f34f/x86_64-centos9-gcc11-opt/lib \
-	-Wl,-rpath,/cvmfs/sft.cern.ch/lcg/releases/MCGenerators/pythia8/308-3f34f/x86_64-centos9-gcc11-opt/lib \
+	-L$PYTHIA8/lib \
+	-Wl,-rpath,$PYTHIA8/lib \
 	-lpythia8 -ldl \
 #	-I/cvmfs/sft.cern.ch/lcg/releases/HepMC/2.06.11-d5a39/x86_64-centos9-gcc11-opt/include \
 #	-L/cvmfs/sft.cern.ch/lcg/releases/HepMC/2.06.11-d5a39/x86_64-centos9-gcc11-opt/lib \
