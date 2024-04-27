7456b65f4f4afb2704f109bd50926fd3bec943e5
j-s-ashley <jashley6@vols.utk.edu>
2024-04-27 19:33:15 +0200

    Updates generate.sh to log MadGraph terminal output

--- generate_events/generate.sh
+++ generate_events/generate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /cvmfs/sft.cern.ch/lcg/views/LCG_102b_ATLAS_6/x86_64-centos9-gcc11-opt/setup.sh
-mg5_aMC proc_card.dat
+mg5_aMC proc_card.dat > mg5.log
 gunzip $PWD/PROC_RPVMSSM_UFO_0/Events/run_01/unweighted_events.lhe.gz
 ./main42 main42.cmnd out.hepmc > out.log
