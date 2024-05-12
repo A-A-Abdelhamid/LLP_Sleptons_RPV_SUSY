8c5c99c6d2d773b74540120f04d689323cf0cd3e
j-s-ashley <jashley6@vols.utk.edu>
2024-05-12 14:44:11 +0200

    Removes Pythia compilation script

--- generate_events/repo_setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source /cvmfs/sft.cern.ch/lcg/views/LCG_102b_ATLAS_6/x86_64-centos9-gcc11-opt/setup.sh
-cp /cvmfs/sft.cern.ch/lcg/releases/MCGenerators/pythia8/308-3f34f/x86_64-centos9-gcc11-opt/share/Pythia8/examples/main42.cc main42.cc
-bash make_pythia_script.sh main42
