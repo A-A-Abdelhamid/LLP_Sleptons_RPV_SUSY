baba27a0561b2648dae0f85f254ea7391470ee90
j-s-ashley <jashley6@vols.utk.edu>
2024-05-12 02:18:42 +0200

    Tidies and creates input creation script

---
+++ generate_events/make_input.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+f = open("input.txt", "w")
+
+mass_start = float(input("Enter the minimum particle mass in GeV: "))
+mass_end = float(input("Enter the maximum particle mass in GeV: "))
+mass_increment = float(input("Enter the mass step size in GeV: "))
+num_masses = int( ( (mass_end - mass_start) / mass_increment ) + 1 )
+
+tau_start = float(input("Enter the minimum particle lifetime in ns: "))
+tau_end = float(input("Enter the maximum particle lifetime in ns: "))
+tau_increment = float(input("Enter the lifetime step size in ns: "))
+num_taus = int( ( (tau_end - tau_start) / tau_increment ) + 1 )
+
+print("Creating input card...")
+
+for mass in np.linspace(mass_start, mass_end, num_masses):
+    for tau in np.linspace(tau_start, tau_end, num_taus):
+        f.write( str(mass) + " " + str(tau) + "\n")
+
+f.close()
+
+print("Input card 'input.txt' created.")
